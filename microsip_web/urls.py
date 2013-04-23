aa6b6497e3de6383289770aa3fd285daa80608de
Jesús Herrera <jesusmaherrera@gmail.com>
2013-04-23 15:19:38 -0600

    se habilitaron todos los modulos

--- microsip_web/urls.py
+++ microsip_web/urls.py
@@ -17,16 +17,16 @@ urlpatterns = patterns('',
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 	(r'^$', 'main.views.index'),
     #Descomentar esta linea para habilitar inventarios
-    #url(r'^inventarios/', include('inventarios.urls', namespace='Inventarios')),
+    url(r'^inventarios/', include('inventarios.urls', namespace='Inventarios')),
 
     #Descomentar esta linea para habilitar ventas
-    #url(r'^ventas/', include('ventas.urls', namespace='ventas')),
+    url(r'^ventas/', include('ventas.urls', namespace='ventas')),
 
     #Descomentar esta linea para habilitar cuentas_por_pagar
     url(r'^cuentas_por_pagar/', include('cuentas_por_pagar.urls', namespace='cuentas_por_pagar')),
     
     #Descomentar esta linea para habilitar contabilidad
-    #url(r'^contabilidad/', include('contabilidad.urls', namespace='contabilidad')),
+    url(r'^contabilidad/', include('contabilidad.urls', namespace='contabilidad')),
     
     url(r'autocomplete/', include('autocomplete_light.urls')),
     #url(r'^admin/', include(admin.site.urls)),
