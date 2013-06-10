fb268d2108a22ea71e80ac7b2c626d646abb636f
Jesús Herrera <jesusmaherrera@gmail.com>
2013-06-10 15:56:51 -0600

    validacion de asiento ignora

--- microsip_web/apps/cuentas_por_cobrar/forms.py
+++ microsip_web/apps/cuentas_por_cobrar/forms.py
@@ -9,38 +9,38 @@ from django.forms.models import BaseInlineFormSet, inlineformset_factory
 from microsip_web.apps.cuentas_por_cobrar.models import *
 
 class InformacionContableManageForm(forms.ModelForm):
-	condicion_pago_contado 	= forms.ModelChoiceField(queryset= CondicionPago.objects.all(), required=True)
+    condicion_pago_contado  = forms.ModelChoiceField(queryset= CondicionPago.objects.all(), required=True)
 
-	class Meta:
-		model = InformacionContable_CC
+    class Meta:
+        model = InformacionContable_CC
 
 class GenerarPolizasManageForm(forms.Form):
-	fecha_ini 					= forms.DateField()
-	fecha_fin 					= forms.DateField()
-	ignorar_documentos_cont 	= forms.BooleanField(required=False, initial=True)
-	CREAR_POR = (
-	    ('Documento', 'Documento'),
-	    ('Dia', 'Dia'),
-	    ('Periodo', 'Periodo'),
-	)
-	crear_polizas_por 			= forms.ChoiceField(choices=CREAR_POR)
+    fecha_ini                   = forms.DateField()
+    fecha_fin                   = forms.DateField()
+    ignorar_documentos_cont     = forms.BooleanField(required=False, initial=True)
+    CREAR_POR = (
+        ('Documento', 'Documento'),
+        ('Dia', 'Dia'),
+        ('Periodo', 'Periodo'),
+    )
+    crear_polizas_por           = forms.ChoiceField(choices=CREAR_POR)
 
-	plantilla 					= forms.ModelChoiceField(queryset= PlantillaPolizas_CC.objects.all(), required=True)
-	#plantilla_2 = forms.ModelChoiceField(queryset= PlantillaPolizas_V.objects.all(), required=True)
-	descripcion 				= forms.CharField(max_length=100, required=False)
-	crear_polizas_de 			= forms.ModelChoiceField(queryset= ConceptoCc.objects.filter(crear_polizas='S'), required=True)
+    plantilla                   = forms.ModelChoiceField(queryset= PlantillaPolizas_CC.objects.all(), required=True)
+    #plantilla_2 = forms.ModelChoiceField(queryset= PlantillaPolizas_V.objects.all(), required=True)
+    descripcion                 = forms.CharField(max_length=100, required=False)
+    crear_polizas_de            = forms.ModelChoiceField(queryset= ConceptoCc.objects.filter(crear_polizas='S'), required=True)
 
 
 class PlantillaPolizaManageForm(forms.ModelForm):
-	tipo = forms.ModelChoiceField(queryset= ConceptoCc.objects.filter(crear_polizas='S'), required=True)
-	class Meta:
-		model = PlantillaPolizas_CC
+    tipo = forms.ModelChoiceField(queryset= ConceptoCc.objects.filter(crear_polizas='S'), required=True)
+    class Meta:
+        model = PlantillaPolizas_CC
 
 class ConceptoPlantillaPolizaManageForm(forms.ModelForm):
-	TIPOS 						= (('C', 'Cargo'),('A', 'Abono'),)
-	VALOR_IVA_TIPOS             = (('A', 'Ambos'),('I', 'Solo IVA'),('0', 'Solo 0%'),)
-	VALOR_CONTADO_CREDITO_TIPOS = (('Ambos', 'Ambos'),('Contado', 'Contado'),('Credito', 'Credito'),)
-	VALOR_TIPOS =(
+    TIPOS                       = (('C', 'Cargo'),('A', 'Abono'),)
+    VALOR_IVA_TIPOS             = (('A', 'Ambos'),('I', 'Solo IVA'),('0', 'Solo 0%'),)
+    VALOR_CONTADO_CREDITO_TIPOS = (('Ambos', 'Ambos'),('Contado', 'Contado'),('Credito', 'Credito'),)
+    VALOR_TIPOS =(
         ('Ventas', 'Ventas'),
         ('Clientes', 'Clientes'),
         ('Bancos', 'Bancos'),
@@ -53,23 +53,32 @@ class ConceptoPlantillaPolizaManageForm(forms.ModelForm):
         ('Segmento_5', 'Segmento 5'),
     )
 
-	# tipo 					= forms.ChoiceField(choices=TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
-	# valor_tipo 				= forms.ChoiceField(choices=VALOR_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
-	# valor_iva 				= forms.ChoiceField(choices=VALOR_IVA_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
-	# valor_contado_credito 	= forms.ChoiceField(choices=VALOR_CONTADO_CREDITO_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
+    # tipo                  = forms.ChoiceField(choices=TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
+    # valor_tipo                = forms.ChoiceField(choices=VALOR_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
+    # valor_iva                 = forms.ChoiceField(choices=VALOR_IVA_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
+    # valor_contado_credito     = forms.ChoiceField(choices=VALOR_CONTADO_CREDITO_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
 
-	posicion  		=  forms.RegexField(regex=r'^(?:\+|-)?\d+$', widget=forms.TextInput(attrs={'class':'span1'}), required= False)
-	asiento_ingora 	= forms.RegexField(regex=r'^(?:\+|-)?\d+$', widget=forms.TextInput(attrs={'class':'span1'}), required= False)
+    posicion        =  forms.RegexField(regex=r'^(?:\+|-)?\d+$', widget=forms.TextInput(attrs={'class':'span1'}), required= False)
+    asiento_ingora  = forms.RegexField(regex=r'^(?:\+|-)?\d+$', widget=forms.TextInput(attrs={'class':'span1'}), required= False)
 
-	class Meta:
-		widgets = autocomplete_light.get_widgets_dict(DetallePlantillaPolizas_CC)
-		model = DetallePlantillaPolizas_CC
+    class Meta:
+        widgets = autocomplete_light.get_widgets_dict(DetallePlantillaPolizas_CC)
+        model = DetallePlantillaPolizas_CC
 
-	def clean_cuenta_co(self):
-	    cuenta_co = self.cleaned_data['cuenta_co']
-	    if CuentaCo.objects.filter(cuenta_padre=cuenta_co.id).count() > 1:
-	    	raise forms.ValidationError(u'la cuenta contable (%s) no es de ultimo nivel, por favor seleciona una cuenta de ultimo nivel' % cuenta_co )
-	    return cuenta_co
+    def clean_cuenta_co(self):
+        cuenta_co = self.cleaned_data['cuenta_co']
+        if CuentaCo.objects.filter(cuenta_padre=cuenta_co.id).count() > 1:
+            raise forms.ValidationError(u'la cuenta contable (%s) no es de ultimo nivel, por favor seleciona una cuenta de ultimo nivel' % cuenta_co )
+        return cuenta_co
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        valor_tipo = cleaned_data.get("valor_tipo")
+        asiento_ingora = cleaned_data.get("asiento_ingora")
+        
+        if ('+' in asiento_ingora or '-' in asiento_ingora) and 'Segmento' in valor_tipo:
+            raise forms.ValidationError(u'los Aisetntos tipo segmentno no pueden agregar ni eliminar otros solo sustiturilos')
+        return cleaned_data
+        
 def PlantillaPoliza_items_formset(form, formset = BaseInlineFormSet, **kwargs):
-	return inlineformset_factory(PlantillaPolizas_CC, DetallePlantillaPolizas_CC, form, formset, **kwargs)
+    return inlineformset_factory(PlantillaPolizas_CC, DetallePlantillaPolizas_CC, form, formset, **kwargs)
--- microsip_web/apps/cuentas_por_pagar/forms.py
+++ microsip_web/apps/cuentas_por_pagar/forms.py
@@ -9,44 +9,44 @@ from django.forms.models import BaseInlineFormSet, inlineformset_factory
 from models import *
 
 class ProveedorManageForm(forms.ModelForm):
-	class Meta:
-		widgets = autocomplete_light.get_widgets_dict(Proveedor)
-		model = Proveedor
+    class Meta:
+        widgets = autocomplete_light.get_widgets_dict(Proveedor)
+        model = Proveedor
 
 class InformacionContableManageForm(forms.ModelForm):
-	condicion_pago_contado 	= forms.ModelChoiceField(queryset= CondicionPagoCp.objects.all(), required=True)
+    condicion_pago_contado  = forms.ModelChoiceField(queryset= CondicionPagoCp.objects.all(), required=True)
 
-	class Meta:
-		model = InformacionContable_CP
+    class Meta:
+        model = InformacionContable_CP
 
 class GenerarPolizasManageForm(forms.Form):
-	fecha_ini 				= forms.DateField()
-	fecha_fin 				= forms.DateField()
-	ignorar_documentos_cont 	= forms.BooleanField(required=False, initial=True)
-	CREAR_POR = (
-	    ('Documento', 'Documento'),
-	    ('Dia', 'Dia'),
-	    ('Periodo', 'Periodo'),
-	)
-	crear_polizas_por 		= forms.ChoiceField(choices=CREAR_POR)
+    fecha_ini               = forms.DateField()
+    fecha_fin               = forms.DateField()
+    ignorar_documentos_cont     = forms.BooleanField(required=False, initial=True)
+    CREAR_POR = (
+        ('Documento', 'Documento'),
+        ('Dia', 'Dia'),
+        ('Periodo', 'Periodo'),
+    )
+    crear_polizas_por       = forms.ChoiceField(choices=CREAR_POR)
 
-	plantilla 	= forms.ModelChoiceField(queryset= PlantillaPolizas_CP.objects.all(), required=True)
-	#plantilla_2 = forms.ModelChoiceField(queryset= PlantillaPolizas_V.objects.all(), required=True)
-	descripcion = forms.CharField(max_length=100, required=False)
-	
-	crear_polizas_de 		= forms.ModelChoiceField(queryset= ConceptoCp.objects.filter(crear_polizas='S'), required=True)
+    plantilla   = forms.ModelChoiceField(queryset= PlantillaPolizas_CP.objects.all(), required=True)
+    #plantilla_2 = forms.ModelChoiceField(queryset= PlantillaPolizas_V.objects.all(), required=True)
+    descripcion = forms.CharField(max_length=100, required=False)
+    
+    crear_polizas_de        = forms.ModelChoiceField(queryset= ConceptoCp.objects.filter(crear_polizas='S'), required=True)
 
 
 class PlantillaPolizaManageForm(forms.ModelForm):
-	tipo = forms.ModelChoiceField(queryset= ConceptoCp.objects.filter(crear_polizas='S'), required=True)
-	class Meta:
-		model = PlantillaPolizas_CP
+    tipo = forms.ModelChoiceField(queryset= ConceptoCp.objects.filter(crear_polizas='S'), required=True)
+    class Meta:
+        model = PlantillaPolizas_CP
 
 class ConceptoPlantillaPolizaManageForm(forms.ModelForm):
-	TIPOS 						= (('C', 'Cargo'),('A', 'Abono'),)
-	VALOR_IVA_TIPOS             = (('A', 'Ambos'),('I', 'Solo IVA'),('0', 'Solo 0%'),)
-	VALOR_CONTADO_CREDITO_TIPOS = (('Ambos', 'Ambos'),('Contado', 'Contado'),('Credito', 'Credito'),)
-	VALOR_TIPOS =(
+    TIPOS                       = (('C', 'Cargo'),('A', 'Abono'),)
+    VALOR_IVA_TIPOS             = (('A', 'Ambos'),('I', 'Solo IVA'),('0', 'Solo 0%'),)
+    VALOR_CONTADO_CREDITO_TIPOS = (('Ambos', 'Ambos'),('Contado', 'Contado'),('Credito', 'Credito'),)
+    VALOR_TIPOS =(
         ('Compras', 'Compras'),
         ('Proveedores', 'Proveedores'),
         ('Bancos', 'Bancos'),
@@ -61,23 +61,31 @@ class ConceptoPlantillaPolizaManageForm(forms.ModelForm):
         ('Segmento_5', 'Segmento 5'),
     )
 
-	# tipo 					= forms.ChoiceField(choices=TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
-	# valor_tipo 				= forms.ChoiceField(choices=VALOR_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
-	# valor_iva 				= forms.ChoiceField(choices=VALOR_IVA_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
-	# valor_contado_credito 	= forms.ChoiceField(choices=VALOR_CONTADO_CREDITO_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
+    # tipo                  = forms.ChoiceField(choices=TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
+    # valor_tipo                = forms.ChoiceField(choices=VALOR_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
+    # valor_iva                 = forms.ChoiceField(choices=VALOR_IVA_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
+    # valor_contado_credito     = forms.ChoiceField(choices=VALOR_CONTADO_CREDITO_TIPOS, widget=forms.Select(attrs={'class':'span2'}),)
 
-	posicion  		=  forms.RegexField(regex=r'^(?:\+|-)?\d+$', widget=forms.TextInput(attrs={'class':'span1'}), required= False)
-	asiento_ingora 	= forms.RegexField(regex=r'^(?:\+|-)?\d+$', widget=forms.TextInput(attrs={'class':'span1'}), required= False)
+    posicion        =  forms.RegexField(regex=r'^(?:\+|-)?\d+$', widget=forms.TextInput(attrs={'class':'span1'}), required= False)
+    asiento_ingora  = forms.RegexField(regex=r'^(?:\+|-)?\d+$', widget=forms.TextInput(attrs={'class':'span1'}), required= False)
 
-	class Meta:
-		widgets = autocomplete_light.get_widgets_dict(DetallePlantillaPolizas_CP)
-		model = DetallePlantillaPolizas_CP
+    class Meta:
+        widgets = autocomplete_light.get_widgets_dict(DetallePlantillaPolizas_CP)
+        model = DetallePlantillaPolizas_CP
 
-	def clean_cuenta_co(self):
-	    cuenta_co = self.cleaned_data['cuenta_co']
-	    if CuentaCo.objects.filter(cuenta_padre=cuenta_co.id).count() > 1:
-	    	raise forms.ValidationError(u'la cuenta contable (%s) no es de ultimo nivel, por favor seleciona una cuenta de ultimo nivel' % cuenta_co )
-	    return cuenta_co
+    def clean_cuenta_co(self):
+        cuenta_co = self.cleaned_data['cuenta_co']
+        if CuentaCo.objects.filter(cuenta_padre=cuenta_co.id).count() > 1:
+            raise forms.ValidationError(u'la cuenta contable (%s) no es de ultimo nivel, por favor seleciona una cuenta de ultimo nivel' % cuenta_co )
+        return cuenta_co
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        valor_tipo = cleaned_data.get("valor_tipo")
+        asiento_ingora = cleaned_data.get("asiento_ingora")
+        
+        if ('+' in asiento_ingora or '-' in asiento_ingora) and 'Segmento' in valor_tipo:
+            raise forms.ValidationError(u'los Aisetntos tipo segmentno no pueden agregar ni eliminar otros solo sustiturilos')
+        return cleaned_data
 
 def PlantillaPoliza_items_formset(form, formset = BaseInlineFormSet, **kwargs):
-	return inlineformset_factory(PlantillaPolizas_CP, DetallePlantillaPolizas_CP, form, formset, **kwargs)
+    return inlineformset_factory(PlantillaPolizas_CP, DetallePlantillaPolizas_CP, form, formset, **kwargs)
