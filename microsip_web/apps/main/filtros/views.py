8e512607baf3bdd13bde29b1a5f01ebaa6c23289
Jesus Baray Legarda <jbarayl@hotmail.com>
2013-07-24 16:45:49 -0600

    GENERADOR DE CARPETAS APP FILTROS

--- microsip_web/apps/main/filtros/views.py
+++ microsip_web/apps/main/filtros/views.py
@@ -3,7 +3,7 @@ from django.db import connection
 def get_next_id_carpeta():
     """ return next value of sequence """
     c = connection.cursor()
-    c.execute('SELECT NEXT VALUE FOR "MAIN_CARPETA_SQ" FROM RDB$DATABASE;')
+    c.execute('SELECT NEXT VALUE FOR "SIC_CARPETA_SQ" FROM RDB$DATABASE;')
     row = c.fetchone()
     return int(row[0])
 
