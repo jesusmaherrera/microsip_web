c78522cadeab54708a3e510d289df178d564d203
Jesús Herrera <jesusmaherrera@gmail.com>
2013-08-08 17:41:08 -0600

    se modifico trigers inventarios

--- microsip_web/apps/inventarios/triggers.py
+++ microsip_web/apps/inventarios/triggers.py
@@ -12,12 +12,10 @@ triggers['SIC_PUERTA_INV_DOCTOSIN_BU'] = '''
 
     declare variable inv_articulo_id integer;
     declare variable inv_articulo_unidades double PRECISION;
-    declare variable inv_detalle_id integer;
 
-    declare variable art_discreto_id integer;
     declare variable almacen_id integer;
-    declare variable inv_tipo char(1);
     declare variable seguimiento char(1);
+    declare variable inv_detalle_tipomovto char(1);
 
     declare variable docto_invfis_fecha date;
     declare variable docto_in_fecha date;
@@ -28,38 +26,36 @@ triggers['SIC_PUERTA_INV_DOCTOSIN_BU'] = '''
         if (new.cancelado='S') then
         begin
             /*Datos de documento in*/
-            select first 1 doctos_in.naturaleza_concepto, doctos_in.almacen_id, doctos_in.fecha
+            select first 1  doctos_in.fecha
             from doctos_in
             where doctos_in.docto_in_id = new.docto_in_id
-            INTO :inv_tipo, :almacen_id, :docto_in_fecha;
+            INTO :docto_in_fecha;
         
-            /*Datos de inventario fisico abierto*/
-            select first 1 doctos_invfis.docto_invfis_id, doctos_invfis.fecha
-            from doctos_invfis where doctos_invfis.aplicado ='N' and doctos_invfis.almacen_id= :almacen_id
-            INTO :invfis_id, :docto_invfis_fecha;
-        
-            /*Si existe un inventario fisico [abierto] del almacen del cual es este documento*/
-            if (not invfis_id is null and docto_in_fecha >= docto_invfis_fecha) then
+            /*Se recorren todos los detalles del documento[entrada/salida]*/
+            for select articulo_id, unidades, tipo_movto, almacen_id
+            from doctos_in_det
+            where docto_in_id = new.docto_in_id
+            into :inv_articulo_id, :inv_articulo_unidades, :inv_detalle_tipomovto, :almacen_id
+            do
             begin
-                /*Se recorren todos los detalles del documento[entrada/salida]*/
-                for select articulo_id, unidades, docto_in_det_id
-                from doctos_in_det
-                where docto_in_id = new.docto_in_id
-                into :inv_articulo_id, :inv_articulo_unidades, :inv_detalle_id
-                do
+                /*Datos de inventario fisico abierto*/
+                select first 1 doctos_invfis.docto_invfis_id, doctos_invfis.fecha
+                from doctos_invfis where doctos_invfis.aplicado ='N' and doctos_invfis.almacen_id= :almacen_id
+                INTO :invfis_id, :docto_invfis_fecha;
+        
+                /*Si existe un inventario fisico [abierto] del almacen del cual es este documento*/
+                if (not invfis_id is null and docto_in_fecha >= docto_invfis_fecha) then
                 begin
                     select docto_invfis_det_id, unidades
                     from doctos_invfis_det
                     where docto_invfis_id = :invfis_id and articulo_id =:inv_articulo_id
                     into :invfis_det_id, :invfis_articulo_unidades;
 
-
                     if (not invfis_det_id is null) then
                     begin
-
-                        if(inv_tipo='E') then
+                        if(inv_detalle_tipomovto = 'E') then
                             invfis_articulo_unidades = invfis_articulo_unidades - inv_articulo_unidades;
-                        else if (inv_tipo='S') then
+                        else if (inv_detalle_tipomovto = 'S') then
                         begin
                             select seguimiento from articulos where articulo_id = :inv_articulo_id
                             into :seguimiento;
@@ -71,9 +67,10 @@ triggers['SIC_PUERTA_INV_DOCTOSIN_BU'] = '''
                             invfis_articulo_unidades = 0;
                         update doctos_invfis_det set unidades= :invfis_articulo_unidades where docto_invfis_det_id = :invfis_det_id;
                     end
-
                 end
             end
+
+
         end
     end
     '''
@@ -96,17 +93,18 @@ triggers['SIC_PUERTA_INV_DESGLOSEDIS_AI'] = '''
     declare variable invfis_id integer;
     declare variable desglose_discr_invfis_id integer;
     declare variable articulo_clave char(20);
-    declare variable naturaleza_concepto char(1);
     declare variable cantidad_articulos double PRECISION;
     declare variable docto_invfis_fecha date;
     declare variable docto_in_fecha date;
+    declare variable inv_detalle_tipomovto char(1);
+
     begin
-        select doctos_in.almacen_id, doctos_in_det.articulo_id, doctos_in.naturaleza_concepto, doctos_in.fecha
+        select doctos_in.almacen_id, doctos_in_det.articulo_id, doctos_in_det.tipo_movto, doctos_in.fecha
         from doctos_in_det,doctos_in
         where
             doctos_in.docto_in_id = doctos_in_det.docto_in_id and
             doctos_in_det.docto_in_det_id = new.docto_in_det_id
-        into :almacen_id, :articulo_id, :naturaleza_concepto, :docto_in_fecha;
+        into :almacen_id, :articulo_id, :inv_detalle_tipomovto, :docto_in_fecha;
 
         /*clave del articulo*/
         select first 1 clave_articulo from claves_articulos where articulo_id = :articulo_id into :articulo_clave;
@@ -123,7 +121,7 @@ triggers['SIC_PUERTA_INV_DESGLOSEDIS_AI'] = '''
             if (not invfis_id is null) then
             begin
                 /*Si es una entrada*/
-                if (naturaleza_concepto = 'E') then
+                if (inv_detalle_tipomovto = 'E') then
                 begin
                     /*Si no existe el detalle de inventario fisico se crea este*/
                     if (docto_invfis_det_id is null) then
@@ -140,7 +138,7 @@ triggers['SIC_PUERTA_INV_DESGLOSEDIS_AI'] = '''
                         insert into desglose_en_discretos_invfis values(-1, :docto_invfis_det_id, new.art_discreto_id, new.unidades);
                 end
                 /*Si es una salida*/
-                else if  (naturaleza_concepto = 'S')then
+                else if  (inv_detalle_tipomovto = 'S')then
                 begin
                     select first 1 desgl_discreto_invfis_id from desglose_en_discretos_invfis
                     where docto_invfis_det_id=:docto_invfis_det_id and art_discreto_id = new.art_discreto_id
@@ -173,56 +171,112 @@ triggers['SIC_PUERTA_INV_DESGLOSEDIS_AI'] = '''
 
 triggers['SIC_PUERTA_INV_DOCTOSINDET_BI'] = ''' 
     CREATE OR ALTER TRIGGER SIC_PUERTA_INV_DOCTOSINDET_BI FOR DOCTOS_IN_DET
-    ACTIVE BEFORE INSERT POSITION 0
+    ACTIVE BEFORE INSERT OR UPDATE POSITION 0
     AS
     declare variable invfis_id integer;
+
     declare variable invfis_det_id integer;
-    declare variable articulo_id integer;
     declare variable cantidad_articulos double PRECISION;
-    declare variable almacen_id integer;
-    declare variable docto_in_tipo char(1);
+
+    declare variable invfis_det__old_id integer;
+    declare variable cantidad_articulos_old double PRECISION;
+
     declare variable seguimiento char(1);
     declare variable docto_invfis_fecha date;
     declare variable docto_in_fecha date;
+    declare variable old_unidades double PRECISION;
 
     begin
         /*Datos de documento in*/
-        select first 1 doctos_in.naturaleza_concepto, doctos_in.almacen_id, doctos_in.fecha
+        select first 1 doctos_in.fecha
         from doctos_in
         where doctos_in.docto_in_id = new.docto_in_id
-        INTO :docto_in_tipo, :almacen_id, :docto_in_fecha;
+        INTO :docto_in_fecha;
 
         /*Datos de inventario fisico abierto*/
         select first 1 doctos_invfis.docto_invfis_id, doctos_invfis.fecha
-        from doctos_invfis where doctos_invfis.aplicado ='N' and doctos_invfis.almacen_id= :almacen_id
+        from doctos_invfis where doctos_invfis.aplicado ='N' and doctos_invfis.almacen_id= new.almacen_id
         INTO :invfis_id, :docto_invfis_fecha;
     
         if (not invfis_id is null and docto_in_fecha >= docto_invfis_fecha) then
         begin
-            select doctos_invfis_det.docto_invfis_det_id, doctos_invfis_det.articulo_id, doctos_invfis_det.unidades
+            select doctos_invfis_det.docto_invfis_det_id, doctos_invfis_det.unidades
             from doctos_invfis, doctos_invfis_det
             where
                 doctos_invfis.docto_invfis_id = doctos_invfis_det.docto_invfis_id and
                 doctos_invfis.docto_invfis_id = :invfis_id and
                 doctos_invfis_det.articulo_id = new.articulo_id
-            into :invfis_det_id, :articulo_id, :cantidad_articulos;
+            into :invfis_det_id, :cantidad_articulos;
 
             if (new.unidades is null) then
                 new.unidades = 0;
             if (cantidad_articulos is null) then
                 cantidad_articulos = 0;
-                                
+            if (old.unidades is null) then
+                old_unidades = 0;
+            else
+                old_unidades = old.unidades;
+
+
             if (not invfis_det_id is null) then
             begin
-                if (docto_in_tipo='E') then
-                    cantidad_articulos = cantidad_articulos + new.unidades;
-                else if (docto_in_tipo='S') then
+                if (new.tipo_movto = 'E') then
+                begin
+                    if (inserting) then
+                        cantidad_articulos = cantidad_articulos + new.unidades - old_unidades;
+                    else
+                    begin
+                        if (old.articulo_id = new.articulo_id) then
+                            cantidad_articulos = cantidad_articulos + new.unidades - old_unidades;
+                        else
+                        begin
+                            cantidad_articulos = cantidad_articulos + new.unidades;
+                            select doctos_invfis_det.docto_invfis_det_id, doctos_invfis_det.unidades
+                            from doctos_invfis, doctos_invfis_det
+                            where
+                                doctos_invfis.docto_invfis_id = doctos_invfis_det.docto_invfis_id and
+                                doctos_invfis.docto_invfis_id = :invfis_id and
+                                doctos_invfis_det.articulo_id = old.articulo_id
+                            into :invfis_det__old_id, :cantidad_articulos_old;
+
+                            cantidad_articulos_old = cantidad_articulos_old - old_unidades;
+
+                            update doctos_invfis_det set unidades= :cantidad_articulos_old where docto_invfis_det_id = :invfis_det__old_id;
+                        end
+                    end
+                end
+                else if (new.tipo_movto = 'S') then
                 begin
                     select seguimiento from articulos where articulo_id = new.articulo_id
                     into :seguimiento;
-                    
+
                     if (seguimiento <> 'S') then
-                        cantidad_articulos = cantidad_articulos - new.unidades;
+                    begin
+                        if (inserting) then
+                            cantidad_articulos = cantidad_articulos - new.unidades + old_unidades;
+                        else
+                        begin
+                            if (old.articulo_id = new.articulo_id) then
+                            begin
+                                cantidad_articulos = cantidad_articulos - new.unidades + old_unidades;
+                            end
+                            else
+                            begin
+                                cantidad_articulos = cantidad_articulos - new.unidades;
+                                select doctos_invfis_det.docto_invfis_det_id, doctos_invfis_det.unidades
+                                from doctos_invfis, doctos_invfis_det
+                                where
+                                    doctos_invfis.docto_invfis_id = doctos_invfis_det.docto_invfis_id and
+                                    doctos_invfis.docto_invfis_id = :invfis_id and
+                                    doctos_invfis_det.articulo_id = old.articulo_id
+                                into :invfis_det__old_id, :cantidad_articulos_old;
+
+                                cantidad_articulos_old = cantidad_articulos_old + old_unidades;
+
+                                update doctos_invfis_det set unidades= :cantidad_articulos_old where docto_invfis_det_id = :invfis_det__old_id;
+                            end
+                        end
+                    end
                 end
 
                 if (cantidad_articulos < 0 ) then
@@ -232,7 +286,7 @@ triggers['SIC_PUERTA_INV_DOCTOSINDET_BI'] = '''
             end
             else
             begin
-                if (docto_in_tipo='E') then
+                if (NEW.tipo_movto = 'E') then
                     insert into doctos_invfis_det (docto_invfis_det_id, docto_invfis_id, clave_articulo, articulo_id, unidades)
                         values(-1, :invfis_id, new.clave_articulo, new.articulo_id, new.unidades);
             end
@@ -249,18 +303,16 @@ triggers['SIC_PUERTA_INV_DOCTOSINDET_BD'] = '''
     declare variable articulo_id integer;
     declare variable cantidad_articulos double PRECISION;
     declare variable articulo_discreto_id integer;
-    declare variable docto_in_tipo char(1);
-    declare variable articulo_seguimiento char(1);
     declare variable docto_invfis_fecha date;
     declare variable docto_in_fecha date;
 
     begin
 
         /*Datos de documento in*/
-        select first 1 doctos_in.naturaleza_concepto, doctos_in.fecha
+        select first 1 doctos_in.fecha
         from doctos_in
         where doctos_in.docto_in_id = old.docto_in_id
-        INTO :docto_in_tipo, :docto_in_fecha;
+        INTO :docto_in_fecha;
     
         /*Datos de inventario fisico abierto*/
         select first 1 doctos_invfis.docto_invfis_id, doctos_invfis.fecha
@@ -271,16 +323,16 @@ triggers['SIC_PUERTA_INV_DOCTOSINDET_BD'] = '''
         begin
 
             for
-                select doctos_invfis_det.docto_invfis_det_id, doctos_invfis_det.articulo_id, doctos_invfis_det.unidades, articulos.seguimiento
+                select doctos_invfis_det.docto_invfis_det_id, doctos_invfis_det.articulo_id, doctos_invfis_det.unidades
                 from doctos_invfis_det, articulos
                 where
                     doctos_invfis_det.docto_invfis_id =  :invfis_id and
                     doctos_invfis_det.articulo_id = articulos.articulo_id
-                into :invfis_det_id, :articulo_id, :cantidad_articulos, articulo_seguimiento
+                into :invfis_det_id, :articulo_id, :cantidad_articulos
             do
             begin
 
-                if (articulo_id = old.articulo_id and docto_in_tipo='E') then
+                if (articulo_id = old.articulo_id and old.tipo_movto = 'E') then
                 begin
                     cantidad_articulos = cantidad_articulos - old.unidades;
 
@@ -294,7 +346,7 @@ triggers['SIC_PUERTA_INV_DOCTOSINDET_BD'] = '''
                         ) and
                         docto_invfis_det_id = :invfis_det_id;
                 end
-                else if (articulo_id = old.articulo_id and docto_in_tipo='S') then
+                else if (articulo_id = old.articulo_id and old.tipo_movto = 'S') then
                 begin
                     cantidad_articulos = cantidad_articulos + old.unidades;
 
