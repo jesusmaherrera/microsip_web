5c7539b79252a37ed1d97620829b147e137cc9d8
Jesús Herrera <jesusmaherrera@gmail.com>
2013-08-12 10:30:39 -0600

    prevent doble click submitions

--- microsip_web/apps/main/templatetags/main_menu_tags.py
+++ microsip_web/apps/main/templatetags/main_menu_tags.py
@@ -10,17 +10,17 @@ def microsip_module_btn(ms_module):
 
    if ms_module in settings.MICROSIP_MODULES:
       if ms_module == 'microsip_web.apps.ventas':
-         result = '<li><a href="/ventas/Facturas/"> <i class="msicon-ventas"></i><label class="labelMenu"> Ventas</label></a></li>'
+         result = '<li><a href="/ventas/Facturas/"> <i class="msicon-ventas"></i><label class="labelMenu"> </label></a></li>'
       elif ms_module == 'microsip_web.apps.inventarios':
-         result ='<li><a href="/inventarios/InventariosFisicos/"> <i class="msicon-inventarios"></i><label class="labelMenu"> Inventarios</label></a></li>'
+         result ='<li><a href="/inventarios/InventariosFisicos/"> <i class="msicon-inventarios"></i></a></li>'
       elif ms_module == 'microsip_web.apps.cuentas_por_pagar':
-         result ='<li><a  href="/cuentas_por_pagar/GenerarPolizas/"> <i class="msicon-cuentas_por_pagar"></i><label class="labelMenu"> Cuentas por pagar</label></a></li>'
+         result ='<li><a  href="/cuentas_por_pagar/GenerarPolizas/"> <i class="msicon-cuentas_por_pagar"></i><label class="labelMenu"> </label></a></li>'
       elif ms_module == 'microsip_web.apps.cuentas_por_cobrar':
-         result = '<li><a href="/cuentas_por_cobrar/GenerarPolizas/"> <i class="msicon-cuentas_por_cobrar"></i><label class="labelMenu"> Cuentas por cobrar</label></a></li>'
+         result = '<li><a href="/cuentas_por_cobrar/GenerarPolizas/"> <i class="msicon-cuentas_por_cobrar"></i><label class="labelMenu"> </label></a></li>'
       elif ms_module == 'microsip_web.apps.contabilidad':
-         result = '<li><a href="/contabilidad/polizas_pendientes/"> <i class="msicon-contabilidad"></i><label class="labelMenu"> Contabilidad</label></a></li>'
+         result = '<li><a href="/contabilidad/polizas_pendientes/"> <i class="msicon-contabilidad"></i><label class="labelMenu"> </label></a></li>'
       elif ms_module == 'microsip_web.apps.punto_de_venta':
-         result ='<li><a href="/punto_de_venta/ventas/"> <i class="msicon-punto_de_venta"></i><label class="labelMenu"> Punto de venta</label></a></li>'
+         result ='<li><a href="/punto_de_venta/ventas/"> <i class="msicon-punto_de_venta"></i><label class="labelMenu"> </label></a></li>'
       elif ms_module == 'microsip_web.apps.compras':
          result = '<li><a href="#"> <i class="msicon-compras"></i><label class="labelMenu"> Compras</label></a></li>'
    
--- microsip_web/libs/custom_db/procedures.py
+++ microsip_web/libs/custom_db/procedures.py
@@ -22,7 +22,7 @@ procedures['SIC_DOCTOINVFISDET_AT'] = '''
         if (not exists(
         select 1 from RDB$RELATION_FIELDS rf
         where rf.RDB$RELATION_NAME = 'DOCTOS_INVFIS_DET' and rf.RDB$FIELD_NAME = 'SIC_DETALLE_MODIFICACIONES')) then
-            execute statement 'ALTER TABLE DOCTOS_INVFIS_DET ADD SIC_DETALLE_MODIFICACIONES VARCHAR(200)';
+            execute statement 'ALTER TABLE DOCTOS_INVFIS_DET ADD SIC_DETALLE_MODIFICACIONES VARCHAR(400)';
     END  
     '''
 
