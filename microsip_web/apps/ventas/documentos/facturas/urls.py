3f2147dc392f1246837c81c5a691a7293f44bbf4
Jesús Herrera <jesusmaherrera@gmail.com>
2014-03-14 17:08:11 -0700

    cargar factura con datos de remision

--- microsip_web/apps/ventas/documentos/facturas/urls.py
+++ microsip_web/apps/ventas/documentos/facturas/urls.py
@@ -3,6 +3,6 @@ from .views import facturas_view, factura_manageView
 
 urlpatterns = patterns('',
 	(r'^facturas/$', facturas_view),
-	(r'^factura/$', factura_manageView),
 	(r'^factura/(?P<id>\d+)/$', factura_manageView),
+	(r'^factura/(?P<type>[RF])/(?P<id>\d+)/', factura_manageView),
 )
--- microsip_web/apps/ventas/documentos/remisiones/urls.py
+++ microsip_web/apps/ventas/documentos/remisiones/urls.py
@@ -3,6 +3,6 @@ from .views import remisiones_view, remision_manageview
 
 urlpatterns = patterns('',
 	(r'^remisiones/$', remisiones_view),
-	(r'^remision/$', remision_manageview),
+	# (r'^remision/$', remision_manageview),
 	(r'^remision/(?P<id>\d+)/$', remision_manageview),
 )
